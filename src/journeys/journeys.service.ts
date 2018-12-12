f1610dbc62e104a548535e0dfe9e93b5c0751225
p.leroux <pleroux@student.42.fr>
2018-12-12 17:32:42 +0100

    up

--- src/journeys/journeys.service.ts
+++ src/journeys/journeys.service.ts
@@ -2,31 +2,45 @@ import { Component } from '@nestjs/common';
 import { Ijourney } from './interfaces/journey.interface';
 import request = require('request');
 import { TGVMAX_URL } from '../constants';
+import moment from 'moment';
 
 @Component()
 export class JourneysService {
     private train: Ijourney[] = [];
 
-    async findAll(origin: string, destination: string): Promise<Ijourney[]> {
+    // doesn't work until I turn on promise
+    async findAll(origin: string, destination: string, date: string): Promise<Ijourney[]> {
         return new Promise<Ijourney[]>((resolve, reject) => {
+            let dateBegin: string = moment(date).hour(0).minute(0).second(0).toISOString(true);
+            const dateEnd: string = moment(date).hour(23).minute(59).second(59).toISOString(true);
             const token = ''; // set the same fct
-            request.get(TGVMAX_URL, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'ValidityToken': token,
-                },
-            }, (err, rep, body) => {
-                if (err === null && rep && rep.statusCode === 200) {
-                    const data: Ijourney[] = JSON.parse(body);
-                    data.forEach((element, idx) => {
-                        this.train.push(element);
-                        if (idx === data.length - 1) {
-                            element.departureDateTime
-                            // find here how handle only the date format and not the timecode
+            let loop: boolean = true;
+            while (loop) {
+                request.get(TGVMAX_URL + origin + '/' + destination + '/' + dateBegin + '/' + dateEnd, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'ValidityToken': token,
+                    },
+                }, (err, rep, body) => {
+                    if (err === null && rep && rep.statusCode === 200) {
+                        const data: Ijourney[] = JSON.parse(body);
+                        if (data.length) {
+                            data.forEach((element, idx) => {
+                                this.train.push(element);
+                                if (idx === data.length - 1) {
+                                    const dateTmp = moment(element.dpartureDateTime);
+                                    dateBegin = dateTmp.hour(dateTmp.hour())
+                                        .minute(dateTmp.minute())
+                                        .second(dateTmp.second())
+                                        .add(1, 'second')
+                                        .toISOString();
+                                }
+                            });
                         }
-                    });
-                }
-            });
+                        loop = false;
+                    }
+                });
+            }
         });
     }
 }
