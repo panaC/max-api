37f767fa463c6262637add67d1faeaf72d3443e9
pleroux <p.leroux@gmx.com>
2018-12-13 17:24:41 +0100

    fix journeys api endpoint , works fine

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,5 @@ export const TICKET_MODEL_PROVIDER = 'PostModelToken';
 export const DB_URI = 'mongodb://u1nrl6g2ukphl8p:PXFC52rAOZrwD7ikmbe8@b6ciar3dlwmwihf-mongodb.services.clever-cloud.com:27017/b6ciar3dlwmwihf';
 
 export const TGVMAX_URL = 'https://sncf-simulateur-api-prod.azurewebsites.net/api/RailAvailability/Search/';
+
+export const TOKEN_URL = 'https://simulateur.tgvmax.fr/VSC/#/1/';
--- src/journeys/journeys.service.ts
+++ src/journeys/journeys.service.ts
@@ -1,7 +1,7 @@
 import { Component, Header } from '@nestjs/common';
 import { Ijourney } from './interfaces/journey.interface';
 import request = require('request');
-import { TGVMAX_URL } from '../constants';
+import { TGVMAX_URL, TOKEN_URL } from '../constants';
 import * as moment from 'moment';
 
 @Component()
@@ -27,14 +27,36 @@ export class JourneysService {
         });
     }
 
-    // doesn't work until I turn it into promise
+    // no needed now because moment has a UTC fct that doesn't handle Date
+    // use this fct for passthrought the local timezone on a string Date
+    private dateConvert(date: string): string {
+        const d = new Date(date);
+        return new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDay(), d.getHours(), d.getMinutes(), d.getSeconds())).toISOString();
+    }
+
+    private getToken(): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            request(TOKEN_URL, (err, rep, body) => {
+                if (err === null && rep && rep.statusCode === 200) {
+                    if (/input.*id="hiddenToken".*value=\"(.*)\"/g.exec(body)) {
+                        resolve(/input.*id="hiddenToken".*value=\"(.*)\"/g.exec(body)[1]);
+                    }
+                }
+                if (err) { reject('getToken error ' + JSON.stringify(err)); } else {
+                    reject('getToken error ' + rep.statusCode);
+                }
+            });
+        });
+    }
+
     async findAll(origin: string, destination: string, date: string): Promise<Ijourney[]> {
-        let dateBegin: string = moment(date).hour(0).minute(0).second(0).toISOString(true);
-        const dateEnd: string = moment(date).hour(23).minute(59).second(59).toISOString(true);
-        const token = ''; // set the same fct
+        let dateBegin: string = moment.utc(date).hour(0).minute(0).second(0).toISOString();
+        const dateEnd: string = moment.utc(date).hour(23).minute(59).second(59).toISOString();
+        const token = await this.getToken();
         let loop: boolean = true;
         let count: number = 0;
         let data: Ijourney[] = null;
+        this.train = []; // Reset the history of day search , but in the future cache this in db
         while (loop && count < 10) {
             data = await this.myRequest(TGVMAX_URL + origin + '/' + destination + '/' + dateBegin + '/' + dateEnd,
                 {
@@ -45,7 +67,7 @@ export class JourneysService {
                 data.forEach((element, idx) => {
                     this.train.push(element);
                     if (idx === data.length - 1) {
-                        const dateTmp = moment(element.departureDateTime);
+                        const dateTmp = moment.utc(element.departureDateTime);
                         dateBegin = dateTmp.hour(dateTmp.hour())
                             .minute(dateTmp.minute())
                             .second(dateTmp.second())
